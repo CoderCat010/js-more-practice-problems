917351b6100174e23b2ea71224310e3cdd9be4fe
CoderCat010 <nusraumme@gmail.com>
2026-01-22 18:00:26 +0600

    added practice-11

--- practice-prob-10.js
+++ practice-prob-10.js
@@ -1,15 +1,15 @@
 // validation
-function details(info){
-    if(typeof info !== 'object'){
-        return "please provide valid object information";
-    }else if(!info.name || !info.age){
-        return "please provide name and age property";
-    }else if(typeof info.name !== 'string'){
-        return 'provide valid string type value'
-    }else if(typeof info.age !== 'number'){
-        return 'provide number type of value'
-    }
+// function details(info){
+//     if(typeof info !== 'object'){
+//         return "please provide valid object information";
+//     }else if(!info.name || !info.age){
+//         return "please provide name and age property";
+//     }else if(typeof info.name !== 'string'){
+//         return 'provide valid string type value'
+//     }else if(typeof info.age !== 'number'){
+//         return 'provide number type of value'
+//     }
 
-    return `My name is: ${info.name} and my age is ${info.age} hehehehehehe`;
-}
-console.log(details('jdjjd'));
+//     return `My name is: ${info.name} and my age is ${info.age} hehehehehehe`;
+// }
+// console.log(details('jdjjd'));
