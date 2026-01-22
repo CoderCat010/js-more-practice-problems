5132a2d3e33bbf1c0dca152493dae2af141e194e
CoderCat010 <nusraumme@gmail.com>
2026-01-22 16:17:19 +0600

    added practice-4

---
+++ practice-prob-8.js
@@ -0,0 +1,14 @@
+//----> use add an multiplication to calculate wood requairments
+// function woodQuantity(chairQuantitiy, tableQuantity, bedQuantity){
+//     const parChairWood = 3;
+//     const parTableWood = 10;
+//     const parBedWood = 50;
+
+//     const totalChairWood = chairQuantitiy * parChairWood;
+//     const totalTableWood = tableQuantity * parTableWood;
+//     const totalBedWood = bedQuantity * parBedWood;
+//     const totalWood = totalChairWood + totalTableWood + totalBedWood;
+
+//     return totalWood;
+// }
+// console.log(woodQuantity(1, 1, 4));
