b976f82b175e5cd9689f1a6c8e16950edd1d56e2
CoderCat010 <nusraumme@gmail.com>
2026-01-22 14:58:54 +0600

    added practice-3

--- practice-prob-7.js
+++ practice-prob-7.js
@@ -1,14 +1,14 @@
-//-----> find who's the tallest? 
-let heights = [166, 170, 175, 178, 183, 185, 187, 190];
+// //-----> find who's the tallest? 
+// let heights = [166, 170, 175, 178, 183, 185, 187, 190];
 
-function getMax(numbers){
-    let max = numbers[0]
-    for(let i = 0; i < numbers.length; i++){
-        if(numbers[i] > max){
-            max = numbers[i];
-        }
-    }
-    return max;
-}
-console.log(getMax(heights));
+// function getMax(numbers){
+//     let max = numbers[0]
+//     for(let i = 0; i < numbers.length; i++){
+//         if(numbers[i] > max){
+//             max = numbers[i];
+//         }
+//     }
+//     return max;
+// }
+// console.log(getMax(heights));
 
